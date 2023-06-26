9dd7bfee9da00aaadc78f46cb9ac642fa2775057
Mateusz Śródka <mateusz.srodka@gmail.com>
2023-06-26 02:06:55 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+# nie potrzebujemy inputu od uzytkownika, mozemy go miec hardcode'owanego
+# z przykladami od prof. Żylińskiego i własnymi
+
+# TODO zrobić funkcję podającą przykład seidlowi (można w pętli kilka przykładów)
+# ewentualnie po prostu input od uzytkownika wybierajacy ktory przyklad
+
+# potrzebujemy klasy na
+# - funkcję celu
+# - ograniczenia
+# reszta wyjdzie w praniu
